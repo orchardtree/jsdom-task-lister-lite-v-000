6771665769f6409a068a98820f49f84850cf1ec5
Jesse Wenger <wengerj@gmail.com>
2021-06-01 15:58:32 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,7 +3,3 @@ document.addEventListener("DOMContentLoaded", () => {
   event.preventDefault();
 });
 
-document.querySelector("#id-checkbox").addEventListener("click", function(event) {
-         document.getElementById("output-box").innerHTML += "Sorry! <code>preventDefault()</code> won't let you check this!<br>";
-         event.preventDefault();
-}, false);
