a0a1d042029a7310edf1977333f136c022344161
Jesse Wenger <wengerj@gmail.com>
2021-06-01 16:10:12 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   document.querySelector("input[type='submit']").addEventListener("click", function(event) {
-           //document.getElementById("output-box").innerHTML += "Sorry! <code>preventDefault()</code> won't let you check this!<br>";
+           console.log("Sorry! <code>preventDefault()</code> won't let you check this!<br>");
            event.preventDefault();
   }, false);
 });
