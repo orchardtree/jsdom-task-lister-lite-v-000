22727e01294e73062d800e24964fc0c6610999a3
Jesse Wenger <wengerj@gmail.com>
2021-06-01 16:10:40 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector("input[type='submit']").addEventListener("click", function(event) {
+           console.log("Sorry! <code>preventDefault()</code> won't let you check this!<br>");
+           event.preventDefault();
+  }, false);
 });
