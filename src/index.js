1d3f91f704da16e7395fc968600700ba9f2b02f4
Jesse Wenger <wengerj@gmail.com>
2021-06-01 15:54:37 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
   event.preventDefault();
 });
+
+document.querySelector("#id-checkbox").addEventListener("click", function(event) {
+         document.getElementById("output-box").innerHTML += "Sorry! <code>preventDefault()</code> won't let you check this!<br>";
+         event.preventDefault();
+}, false);
